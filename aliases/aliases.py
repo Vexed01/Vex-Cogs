c55a3552d446bdee332d1decf0e71d9f78f6d4a2
Vexed01 <gh.vexed@gmail.com>
2021-02-28 12:22:36 +0000

    [Aliases 1.0.1] Don't error in DMs

--- aliases/aliases.py
+++ aliases/aliases.py
@@ -7,7 +7,7 @@ from redbot.core.utils import deduplicate_iterables
 class Aliases(commands.Cog):
     """Get all the alias information you could ever want about a command"""
 
-    __version__ = "1.0.0"
+    __version__ = "1.0.1"
     __author__ = "Vexed#3211"
 
     def format_help_for_context(self, ctx: commands.Context):
@@ -24,22 +24,28 @@ class Aliases(commands.Cog):
 
     @commands.command()
     async def aliases(self, ctx, *, command: str):
-        """Get all the alias information you could ever want about a command"""
+        """
+        Get all the alias information you could ever want about a command.
+
+        This will show the main command, built-in aliases, global aliases and
+        server aliases.
+        """
         strcommand = command
         command = self.bot.get_command(strcommand)
 
         # meh, safe enough as only reading, there is a big warning on cog install about this
         alias_cog = self.bot.get_cog("Alias")
-        all_guild_aliases = await alias_cog.config.guild(ctx.guild).entries()
         all_global_aliases = await alias_cog.config.entries()
 
         global_aliases = []
         guild_aliases = []
 
-        for alias_cog in all_guild_aliases:
-            if alias_cog["name"] == strcommand:
-                command = self.bot.get_command(alias_cog["command"])
-                guild_aliases.append(alias_cog["name"])
+        if ctx.guild:
+            all_guild_aliases = await alias_cog.config.guild(ctx.guild).entries()
+            for alias_cog in all_guild_aliases:
+                if alias_cog["name"] == strcommand:
+                    command = self.bot.get_command(alias_cog["command"])
+                    guild_aliases.append(alias_cog["name"])
 
         for alias_cog in all_global_aliases:
             if alias_cog["name"] == strcommand:
@@ -49,19 +55,20 @@ class Aliases(commands.Cog):
         if command is None:
             await ctx.send("Hmm, I can't find that command.")
             return
+
         full_com = command.qualified_name
         builtin_aliases = command.aliases
         com_parent = command.parent
 
-        # run again as can miss some in edge cases
-        for alias_cog in all_guild_aliases:
-            if alias_cog["command"] == full_com:
-                guild_aliases.append(alias_cog["name"])
+        if ctx.guild:
+            for alias_cog in all_guild_aliases:
+                if alias_cog["command"] == full_com:
+                    guild_aliases.append(alias_cog["name"])
         for alias_cog in all_global_aliases:
             if alias_cog["command"] == full_com:
                 global_aliases.append(alias_cog["name"])
 
-        # and probs picked up duplicates from second run so:
+        # and probs picked up duplicates on secod run so:
         guild_aliases = deduplicate_iterables(guild_aliases)
         global_aliases = deduplicate_iterables(global_aliases)
 
@@ -86,10 +93,13 @@ class Aliases(commands.Cog):
             list = humanize_list(global_aliases)
             aliases += f"Global aliases: {list}\n"
         if not guild_aliases:
-            none.append("guild")
+            if ctx.guild:
+                none.append("guild")
+            else:
+                aliases += "Your're in DMs, so ther aren't any server aliases."
         else:
             list = humanize_list(guild_aliases)
-            aliases += f"Guild aliases: {list}\n"
+            aliases += f"Server aliases: {list}\n"
 
         none = humanize_list(none, style="or")
 
