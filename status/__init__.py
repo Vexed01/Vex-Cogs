d489df239e7bdf4067be808934fb5743302c6d8a
Vexed01 <gh.vexed@gmail.com>
2021-04-21 17:40:38 +0100

    [Status, CmdLog] Conform to flake8/black

--- status/__init__.py
+++ status/__init__.py
@@ -9,6 +9,5 @@ with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 
-
 def setup(bot: Red) -> None:
     bot.add_cog(Status(bot))
