12ef57b0390e83d001a37e658bd13a30b07e4176
Vexed <gh.vexed@gmail.com>
2021-05-08 15:03:57 +0100

    Restore statusset_com.py

--- status/commands/statusset_com.py
+++ status/commands/statusset_com.py
@@ -204,7 +204,11 @@ class StatusSetCom(MixinMeta):
 
         If you don't specify a channel, I will use the current channel.
         """
-        channel = GuildChannel() if TYPE_CHECKING else chan or ctx.channel
+        if TYPE_CHECKING:
+            channel = GuildChannel()
+        else:
+            channel = chan or ctx.channel
+
         async with self.config.channel(channel).feeds() as feeds:
             if not feeds.pop(service.name, None):
                 return await ctx.send(
@@ -238,7 +242,11 @@ class StatusSetCom(MixinMeta):
         # i basically copied and pasted in rewrite
         # maybe stick the two sections in .utils
 
-        guild = Guild() if TYPE_CHECKING else ctx.guild
+        if TYPE_CHECKING:
+            guild = Guild()
+        else:
+            guild = ctx.guild
+
         unused_feeds = list(FEEDS.keys())
 
         if service:
@@ -251,7 +259,10 @@ class StatusSetCom(MixinMeta):
                         continue
                     mode = settings["mode"]
                     webhook = settings["webhook"]
-                    restrict = channel.id in restrictions.get(service, [])
+                    if channel.id in restrictions.get(service, []):
+                        restrict = True
+                    else:
+                        restrict = False
                     data.append([f"#{channel.name}", mode, webhook, restrict])
 
             table = box(
@@ -404,7 +415,11 @@ class StatusSetCom(MixinMeta):
 
         If you don't specify a channel, I will use the current channel.
         """
-        channel = GuildChannel() if TYPE_CHECKING else chan or ctx.channel
+        if TYPE_CHECKING:
+            channel = GuildChannel()
+        else:
+            channel = chan or ctx.channel
+
         old_conf = await self.config.channel(channel).feeds()
         if service.name not in old_conf.keys():
             return await ctx.send(
@@ -492,7 +507,11 @@ class StatusSetCom(MixinMeta):
         (if there's an incident) members will instead be redirected to channels
         that automatically receive the status updates, that they have permission to to view.
         """
-        channel = GuildChannel() if TYPE_CHECKING else chan or ctx.channel
+        if TYPE_CHECKING:
+            channel = GuildChannel()
+        else:
+            channel = chan or ctx.channel
+
         feed_settings = await self.config.channel(channel).feeds()
         if service.name not in feed_settings.keys():
             return await ctx.send(
