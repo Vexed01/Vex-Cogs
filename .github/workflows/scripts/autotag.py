130acd62f0198d0cc28d399a6b5de78689e3ab19
Vexed01 <gh.vexed@gmail.com>
2021-05-16 19:32:55 +0100

    [FakeCog 1.0.0] Test workflows

--- .github/workflows/scripts/autotag.py
+++ .github/workflows/scripts/autotag.py
@@ -1,4 +1,3 @@
-import json
 import re
 
 from git import Repo
@@ -17,8 +16,6 @@ This is an automatically created tag based of the following commit:
 {commit}
 """
 
-VER_FILE_LOCATION = "api/v1/version.json"
-
 repo = Repo()  # repo in working dir
 
 regex = re.compile(r"\[(\w+) (\d+\.\d+\.\d+)\]")
@@ -36,25 +33,11 @@ else:
         cog = "anotherpingcog"
     ver = match.group(2)
 
-    # tag
     print("Creating and pushing new tag...")
     tag_name = f"{cog}-{ver}"
     repo.create_tag(tag_name, message=TAG_MESSAGE.format(cogname=cog, commit=latest_commit))
     repo.remote().push(f"refs/tags/{tag_name}")
     print(f"Pushed a new tag {tag_name}.")
-
-    # api
-    print("Updating latest version API on GitHub pages...")
-    repo.git.checkout("gh-pages")
-    with open(VER_FILE_LOCATION) as fp:
-        data = json.load(fp)
-    data["cogs"][cog] = ver
-    with open(VER_FILE_LOCATION) as fp:
-        json.dump(data, fp, index=4)
-    repo.index.add([VER_FILE_LOCATION])
-    repo.index.commit(f"Update {cog} ver to {ver}")
-    repo.remote().push()
-    print("Pushed update to the lastest version API.")
 
 
 print("Script finished.")
