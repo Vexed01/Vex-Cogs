5c9999a0a6d12797fa4cdc5fca5859a21ac4c4dc
Sourcery AI <>
2021-06-04 11:47:32 +0000

    'Refactored by Sourcery'

--- status/commands/statusset_com.py
+++ status/commands/statusset_com.py
@@ -208,11 +208,7 @@ class StatusSetCom(MixinMeta):
             - `[p]statusset remove discord #testing`
             - `[p]statusset remove discord` (for using current channel)
         """
-        if TYPE_CHECKING:
-            channel = GuildChannel()
-        else:
-            channel = chan or ctx.channel
-
+        channel = GuildChannel() if TYPE_CHECKING else chan or ctx.channel
         async with self.config.channel(channel).feeds() as feeds:
             if not feeds.pop(service.name, None):
                 return await ctx.send(
@@ -250,11 +246,7 @@ class StatusSetCom(MixinMeta):
         # i basically copied and pasted in rewrite
         # maybe stick the two sections in .utils
 
-        if TYPE_CHECKING:
-            guild = Guild()
-        else:
-            guild = ctx.guild
-
+        guild = Guild() if TYPE_CHECKING else ctx.guild
         unused_feeds = list(FEEDS.keys())
 
         if service:
@@ -267,10 +259,7 @@ class StatusSetCom(MixinMeta):
                         continue
                     mode = settings["mode"]
                     webhook = settings["webhook"]
-                    if channel.id in restrictions.get(service, []):
-                        restrict = True
-                    else:
-                        restrict = False
+                    restrict = channel.id in restrictions.get(service, [])
                     data.append([f"#{channel.name}", mode, webhook, restrict])
 
             table = box(
@@ -431,11 +420,7 @@ class StatusSetCom(MixinMeta):
             - `[p]statusset edit mode #testing discord latest`
             - `[p]statusset edit mode discord edit` (for current channel)
         """
-        if TYPE_CHECKING:
-            channel = GuildChannel()
-        else:
-            channel = chan or ctx.channel
-
+        channel = GuildChannel() if TYPE_CHECKING else chan or ctx.channel
         old_conf = await self.config.channel(channel).feeds()
         if service.name not in old_conf.keys():
             return await ctx.send(
@@ -531,11 +516,7 @@ class StatusSetCom(MixinMeta):
             - `[p]statusset edit restrict #testing discord true`
             - `[p]statusset edit restrict discord false` (for current channel)
         """
-        if TYPE_CHECKING:
-            channel = GuildChannel()
-        else:
-            channel = chan or ctx.channel
-
+        channel = GuildChannel() if TYPE_CHECKING else chan or ctx.channel
         feed_settings = await self.config.channel(channel).feeds()
         if service.name not in feed_settings.keys():
             return await ctx.send(
