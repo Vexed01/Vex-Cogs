86ec5977a30cfedc3d5376c5837d251b579f56dc
Vexed01 <vex@vexcodes.com>
2021-12-30 23:13:02 +0000

    aa

--- .github/workflows/scripts/loadcheck.py
+++ .github/workflows/scripts/loadcheck.py
@@ -2,11 +2,17 @@ import os
 import subprocess
 import time
 
+import requests
 from dotenv import load_dotenv
+from redbot import __version__ as red_str_ver
 
 load_dotenv(".env")
 
 token = os.environ.get("DISCORD_BOT_TOKEN")
+
+python_version = subprocess.check_output(["python", "-V"]).decode("utf-8")
+
+print(f"== Starting Red {red_str_ver} with {python_version}")
 
 proc = subprocess.Popen(
     f"redbot --no-instance --no-prompt --prefix ! --token {token} --rpc",
@@ -17,6 +23,47 @@ proc = subprocess.Popen(
 # let Red boot up
 time.sleep(10)
 
-cogs = ["aliases", "anotherpingcog", "beautify", "betteruptime", "caseinsensitive", "cmdlog"]
+# not compatible with dpy 1.x
+# ["buttonpoll", "ghissues"]
 
-print("done")
+cogs = [
+    "aliases",
+    "anotherpingcog",
+    "beautify",
+    "betteruptime",
+    "caseinsensitive",
+    "cmdlog",
+    "covidgraph",
+    "github",
+    "googletrends",
+    "madtranslate",
+    "stattrack",
+    "status",
+    "system",
+    "timechannel",
+    "wol",
+    #
+    "buttonpoll",
+]
+
+print(f"== Attempting to load {len(cogs)} cogs...")
+
+payload = {
+    "method": "GET_METHODS",
+    # "params": [cogs],
+    "jsonrpc": "2.0",
+    "id": 0,
+}
+
+resp = requests.post("http://localhost:6133/", json=payload)
+
+print("Stopping Red")
+proc.terminate()
+
+print(resp.text)
+print(resp)
+print(resp.json())
+
+# TRY TO FIND A WEBSOCKETS CLIENT
+
+print("== done")
