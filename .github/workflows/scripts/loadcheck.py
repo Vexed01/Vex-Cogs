5a27283a12c80c9e603d0aa2cbb34287da7a815a
Vexed01 <vex@vexcodes.com>
2021-12-31 20:15:08 +0000

    right lets test this on purposeful fail

--- .github/workflows/scripts/loadcheck.py
+++ .github/workflows/scripts/loadcheck.py
@@ -1,9 +1,12 @@
+import asyncio
 import os
 import subprocess
+import sys
 import time
+from typing import Any, Dict, Tuple
 
-import requests
 from dotenv import load_dotenv
+from jsonrpc_websocket import Server
 from redbot import __version__ as red_str_ver
 
 load_dotenv(".env")
@@ -12,12 +15,14 @@ token = os.environ.get("DISCORD_BOT_TOKEN")
 
 python_version = subprocess.check_output(["python", "-V"]).decode("utf-8")
 
-print(f"== Starting Red {red_str_ver} with {python_version}")
+print("=== Red's logs are avalible to view as an Artifact ===\n")
+print(f"Starting Red {red_str_ver} with {python_version}")
 
+file = open("red.log", "w")
 proc = subprocess.Popen(
-    f"redbot --no-instance --no-prompt --prefix ! --token {token} --rpc",
-    # stdout=subprocess.PIPE,
-    # want to let errors go through to logs
+    f"redbot workflow --no-prompt --token {token} --rpc --debug",
+    stdout=file,
+    stderr=subprocess.STDOUT,
 )
 
 # let Red boot up
@@ -46,24 +51,55 @@ cogs = [
     "buttonpoll",
 ]
 
-print(f"== Attempting to load {len(cogs)} cogs...")
 
-payload = {
-    "method": "GET_METHODS",
-    # "params": [cogs],
-    "jsonrpc": "2.0",
-    "id": 0,
-}
+async def leswebsockets() -> Tuple[Dict[str, Any], Dict[str, Any]]:
+    print("Connecting to Red via RPC")
 
-resp = requests.post("http://localhost:6133/", json=payload)
+    server = Server("ws://localhost:6133")
+    try:
+        await server.ws_connect()
+
+        print("Loading cogs")
+        load_results: Dict[str, Any] = await server.CORE__LOAD(cogs)
+        await asyncio.sleep(1)
+        print("Unloading cogs")
+        unload_results: Dict[str, Any] = await server.CORE__UNLOAD(cogs)
+    finally:
+        await server.close()
+
+    return load_results, unload_results
+
+
+load, unload = asyncio.run(leswebsockets())
 
 print("Stopping Red")
+
 proc.terminate()
 
-print(resp.text)
-print(resp)
-print(resp.json())
+exit_code = 0
 
-# TRY TO FIND A WEBSOCKETS CLIENT
+fail_load = []
+for i in (
+    "failed_packages",
+    "invalid_pkg_names",
+    "notfound_packages",
+    "alreadyloaded_packages",
+    "failed_with_reason_packages",
+):
+    fail_load.extend(load[i])
 
-print("== done")
+if fail_load:
+    exit_code = 1
+    print("\N{CROSS MARK} Failed to load cogs " + ", ".join(fail_load))
+    print("See the artifact for more information")
+else:
+    print("\N{HEAVY CHECK MARK} Loaded all cogs successfully")
+
+if unload["failed_packages"]:
+    exit_code = 1
+    print("\N{CROSS MARK} Failed to unload cogs " + ", ".join(unload["failed_packages"]))
+    print("See the artifact for more information")
+else:
+    print("\N{HEAVY CHECK MARK} Unloaded all cogs successfully")
+
+sys.exit(exit_code)
