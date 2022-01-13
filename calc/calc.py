74a441dc51d1513b9947e64621b0ee9420d5fb2a
Vexed01 <vex@vexcodes.com>
2022-01-13 16:29:03 +0000

    add calc cog

---
+++ calc/calc.py
@@ -0,0 +1,38 @@
+from redbot.core import commands
+from redbot.core.bot import Red
+
+from .vexutils import format_help, format_info
+from .view import CalcView
+
+
+class Calc(commands.Cog):
+    """Calculate simple mathematical expressions."""
+
+    __version__ = "0.0.1"
+    __author__ = "Vexed#9000"
+
+    def __init__(self, bot: Red) -> None:
+        self.bot = bot
+
+    def format_help_for_context(self, ctx: commands.Context) -> str:
+        """Thanks Sinbad."""
+        return format_help(self, ctx)
+
+    async def red_delete_data_for_user(self, **kwargs) -> None:
+        """Nothing to delete"""
+        return
+
+    @commands.command(hidden=True)
+    async def calcinfo(self, ctx: commands.Context):
+        await ctx.send(await format_info(ctx, self.qualified_name, self.__version__))
+
+    @commands.command()
+    async def calc(self, ctx: commands.Context):
+        """
+        Show an interactive calculator using buttons.
+        """
+        view = CalcView(self.bot, ctx.author.id)
+        embed = await view.build_embed(await ctx.embed_colour())
+        message = await ctx.send(embed=embed, view=view)
+        view.message = message
+        view.ready.set()
---
+++ calc/view.py
@@ -0,0 +1,291 @@
+import asyncio
+from typing import Optional, Union
+
+import discord
+from discord.colour import Colour
+from discord.enums import ButtonStyle
+from redbot.core.bot import Red
+from redbot.core.utils.chat_formatting import box
+
+from .string_calc import calc_from_string
+
+# yes they dont follow conventions
+# but they are the best string input math library i can find so lets use em
+
+
+class ClosedView(discord.ui.View):
+    @discord.ui.button(label="Calculator closed", style=ButtonStyle.gray, disabled=True)
+    async def btn(self, *args, **kwargs):
+        pass
+
+
+class TimedOutView(discord.ui.View):
+    @discord.ui.button(label="Calculator timed out", style=ButtonStyle.gray, disabled=True)
+    async def btn(self, *args, **kwargs):
+        pass
+
+
+class CalcView(discord.ui.View):
+    """An interactive button-based calculator."""
+
+    def __init__(self, bot: Red, author_id: int, timeout=180):
+        super().__init__(timeout=timeout)
+
+        self.bot = bot
+        self.message: Optional[discord.Message] = None
+        self.author_id = author_id
+
+        self.ready = asyncio.Event()
+
+        self.lazy_edit_task = bot.loop.create_task(self.calc_lazy_edit())
+
+        self.new_edits_avaible = asyncio.Event()
+
+        self.input_reset_ready = True
+
+        self.input = "..."
+        self.output: Union[str, int, float] = "..."
+
+    async def on_timeout(self) -> None:
+        await self.ready.wait()
+        assert self.message is not None
+
+        self.lazy_edit_task.cancel()
+
+        self.stop()
+
+        await self.message.edit(view=TimedOutView())
+
+    async def calc_lazy_edit(self):
+        """Lazily edit the calculator screen, accounting for rate limits."""
+        await self.ready.wait()
+        assert self.message is not None
+
+        while True:
+            await self.new_edits_avaible.wait()
+            await asyncio.sleep(0.7)
+            embed = await self.build_embed()
+            self.new_edits_avaible.clear()
+            await self.message.edit(embed=embed)
+            await asyncio.sleep(1)  # hopefully this is enough... 1.7 wait in total
+
+        # reminder this task is cancelled on timeout and view close via exit button :)
+
+    async def build_embed(self, colour: Colour = None) -> discord.Embed:
+        """Build the embed for the calculator."""
+        if colour is None:
+            await self.ready.wait()
+            assert self.message is not None
+
+        embed = discord.Embed(
+            colour=colour or await self.bot.get_embed_color(self.message.channel),  # type:ignore
+            title="Calculator",
+        )
+        raw_input = self.input
+        friendly_input = raw_input.replace("*", "×").replace("/", "÷")
+        embed.description = (
+            "**Input:**\n" + box(friendly_input) + "\n**Output:**\n" + box(self.output)
+        )
+        # yes i dont handle over 4k embed limit.... but i dont care!! whos gunna do smth that long?
+
+        return embed
+
+    def maybe_update_output(self) -> bool:
+        try:
+            self.output = round(calc_from_string(self.input), 10)
+            return True
+        except SyntaxError:  # not finished typing
+            return False
+        except (ValueError, ZeroDivisionError, OverflowError, RuntimeError):
+            self.output = "Math Error"
+            return False
+
+    async def interaction_check(self, interaction: discord.Interaction) -> bool:
+        await self.ready.wait()
+        assert self.message is not None
+
+        valid = interaction.user.id == self.author_id
+        if not valid:
+            await interaction.response.send_message(
+                "You do not have permission to interact with this calculator.", ephemeral=True
+            )
+        return valid
+
+    @discord.ui.button(label="(", style=discord.ButtonStyle.grey, row=0)
+    async def open_bracket(self, item: discord.ui.Item, interaction: discord.Interaction):
+        if self.input_reset_ready:
+            self.input = ""
+            self.input_reset_ready = False
+        self.input += "("
+        self.maybe_update_output()
+        self.new_edits_avaible.set()
+
+    @discord.ui.button(label=")", style=discord.ButtonStyle.grey, row=0)
+    async def close_bracket(self, item: discord.ui.Item, interaction: discord.Interaction):
+        if self.input_reset_ready:
+            self.input = ""
+            self.input_reset_ready = False
+        self.input += ")"
+        self.maybe_update_output()
+        self.new_edits_avaible.set()
+
+    @discord.ui.button(label="Clear", style=discord.ButtonStyle.danger, row=0)
+    async def clear(self, item: discord.ui.Item, interaction: discord.Interaction):
+        self.input = "..."
+        self.output = "..."
+        self.new_edits_avaible.set()
+        self.input_reset_ready = True
+
+    @discord.ui.button(label="Exit", style=discord.ButtonStyle.danger, row=0)
+    async def exit(self, item: discord.ui.Item, interaction: discord.Interaction):
+        await self.ready.wait()
+        assert self.message is not None
+
+        self.lazy_edit_task.cancel()
+
+        self.stop()
+
+        await self.message.edit(view=ClosedView())
+
+    @discord.ui.button(label="7", style=discord.ButtonStyle.grey, row=1)
+    async def seven(self, item: discord.ui.Item, interaction: discord.Interaction):
+        if self.input_reset_ready:
+            self.input = ""
+            self.input_reset_ready = False
+        self.input += "7"
+        self.maybe_update_output()
+        self.new_edits_avaible.set()
+
+    @discord.ui.button(label="8", style=discord.ButtonStyle.grey, row=1)
+    async def eight(self, item: discord.ui.Item, interaction: discord.Interaction):
+        if self.input_reset_ready:
+            self.input = ""
+            self.input_reset_ready = False
+        self.input += "8"
+        self.maybe_update_output()
+        self.new_edits_avaible.set()
+
+    @discord.ui.button(label="9", style=discord.ButtonStyle.grey, row=1)
+    async def nine(self, item: discord.ui.Item, interaction: discord.Interaction):
+        if self.input_reset_ready:
+            self.input = ""
+            self.input_reset_ready = False
+        self.input += "9"
+        self.maybe_update_output()
+        self.new_edits_avaible.set()
+
+    @discord.ui.button(label="÷", style=discord.ButtonStyle.blurple, row=1)
+    async def divide(self, item: discord.ui.Item, interaction: discord.Interaction):
+        if self.input_reset_ready:
+            self.input = ""
+            self.input_reset_ready = False
+        self.input += "/"
+        self.maybe_update_output()
+        self.new_edits_avaible.set()
+
+    @discord.ui.button(label="4", style=discord.ButtonStyle.grey, row=2)
+    async def four(self, item: discord.ui.Item, interaction: discord.Interaction):
+        if self.input_reset_ready:
+            self.input = ""
+            self.input_reset_ready = False
+        self.input += "4"
+        self.maybe_update_output()
+        self.new_edits_avaible.set()
+
+    @discord.ui.button(label="5", style=discord.ButtonStyle.grey, row=2)
+    async def five(self, item: discord.ui.Item, interaction: discord.Interaction):
+        if self.input_reset_ready:
+            self.input = ""
+            self.input_reset_ready = False
+        self.input += "5"
+        self.maybe_update_output()
+        self.new_edits_avaible.set()
+
+    @discord.ui.button(label="6", style=discord.ButtonStyle.grey, row=2)
+    async def six(self, item: discord.ui.Item, interaction: discord.Interaction):
+        if self.input_reset_ready:
+            self.input = ""
+            self.input_reset_ready = False
+        self.input += "6"
+        self.maybe_update_output()
+        self.new_edits_avaible.set()
+
+    @discord.ui.button(label="×", style=discord.ButtonStyle.blurple, row=2)
+    async def multiply(self, item: discord.ui.Item, interaction: discord.Interaction):
+        if self.input_reset_ready:
+            self.input = ""
+            self.input_reset_ready = False
+        self.input += "*"
+        self.maybe_update_output()
+        self.new_edits_avaible.set()
+
+    @discord.ui.button(label="1", style=discord.ButtonStyle.grey, row=3)
+    async def one(self, item: discord.ui.Item, interaction: discord.Interaction):
+        if self.input_reset_ready:
+            self.input = ""
+            self.input_reset_ready = False
+        self.input += "1"
+        self.maybe_update_output()
+        self.new_edits_avaible.set()
+
+    @discord.ui.button(label="2", style=discord.ButtonStyle.grey, row=3)
+    async def two(self, item: discord.ui.Item, interaction: discord.Interaction):
+        if self.input_reset_ready:
+            self.input = ""
+            self.input_reset_ready = False
+        self.input += "2"
+        self.maybe_update_output()
+        self.new_edits_avaible.set()
+
+    @discord.ui.button(label="3", style=discord.ButtonStyle.grey, row=3)
+    async def three(self, item: discord.ui.Item, interaction: discord.Interaction):
+        if self.input_reset_ready:
+            self.input = ""
+            self.input_reset_ready = False
+        self.input += "3"
+        self.maybe_update_output()
+        self.new_edits_avaible.set()
+
+    @discord.ui.button(label="-", style=discord.ButtonStyle.blurple, row=3)
+    async def minus(self, item: discord.ui.Item, interaction: discord.Interaction):
+        if self.input_reset_ready:
+            self.input = ""
+            self.input_reset_ready = False
+        self.input += "-"
+        self.maybe_update_output()
+        self.new_edits_avaible.set()
+
+    @discord.ui.button(label="0", style=discord.ButtonStyle.grey, row=4)
+    async def zero(self, item: discord.ui.Item, interaction: discord.Interaction):
+        if self.input_reset_ready:
+            self.input = ""
+            self.input_reset_ready = False
+        self.input += "0"
+        self.maybe_update_output()
+        self.new_edits_avaible.set()
+
+    @discord.ui.button(label=".", style=discord.ButtonStyle.grey, row=4)
+    async def decimal(self, item: discord.ui.Item, interaction: discord.Interaction):
+        if self.input_reset_ready:
+            self.input = ""
+            self.input_reset_ready = False
+        self.input += "."
+        self.maybe_update_output()
+        self.new_edits_avaible.set()
+
+    @discord.ui.button(label="=", style=discord.ButtonStyle.green, row=4)
+    async def equals(self, item: discord.ui.Item, interaction: discord.Interaction):
+        if not self.maybe_update_output():
+            self.output = "Math Error"
+        self.input_reset_ready = True
+
+        self.new_edits_avaible.set()
+
+    @discord.ui.button(label="+", style=discord.ButtonStyle.blurple, row=4)
+    async def plus(self, item: discord.ui.Item, interaction: discord.Interaction):
+        if self.input_reset_ready:
+            self.input = ""
+            self.input_reset_ready = False
+        self.input += "+"
+        self.maybe_update_output()
+        self.new_edits_avaible.set()
