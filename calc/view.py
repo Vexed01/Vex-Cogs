21e0084300448823e97072453a369817dcf9e154
Vexed01 <vex@vexcodes.com>
2022-01-14 17:43:17 +0000

    move to expr.py and rearange buttons

--- calc/view.py
+++ calc/view.py
@@ -4,13 +4,13 @@ from typing import Optional, Union
 import discord
 from discord.colour import Colour
 from discord.enums import ButtonStyle
+from expr import EvaluatorError, evaluate
 from redbot.core.bot import Red
 from redbot.core.utils.chat_formatting import box
 
-from .string_calc import calc_from_string
+ZERO_WIDTH = "\u200b"
 
-# yes they dont follow conventions
-# but they are the best string input math library i can find so lets use em
+EQUALS_LABEL = (ZERO_WIDTH + " ") * 16 + "=" + (" " + ZERO_WIDTH) * 15
 
 
 class ClosedView(discord.ui.View):
@@ -67,7 +67,7 @@ class CalcView(discord.ui.View):
             embed = await self.build_embed()
             self.new_edits_avaible.clear()
             await self.message.edit(embed=embed)
-            await asyncio.sleep(1)  # hopefully this is enough... 1.7 wait in total
+            await asyncio.sleep(0.8)  # hopefully this is enough... 1.5 wait in total
 
         # reminder this task is cancelled on timeout and view close via exit button :)
 
@@ -84,7 +84,7 @@ class CalcView(discord.ui.View):
         raw_input = self.input
         friendly_input = raw_input.replace("*", "×").replace("/", "÷")
         embed.description = (
-            "**Input:**\n" + box(friendly_input) + "\n**Output:**\n" + box(self.output)
+            "**Input:**\n" + box(friendly_input) + "\n**Output:**\n" + box(str(self.output))
         )
         # yes i dont handle over 4k embed limit.... but i dont care!! whos gunna do smth that long?
 
@@ -92,11 +92,10 @@ class CalcView(discord.ui.View):
 
     def maybe_update_output(self) -> bool:
         try:
-            self.output = round(calc_from_string(self.input), 10)
+            full_output = evaluate(self.input)
+            self.output = float(round(full_output, 10))
             return True
-        except SyntaxError:  # not finished typing
-            return False
-        except (ValueError, ZeroDivisionError, OverflowError, RuntimeError):
+        except EvaluatorError:
             self.output = "Math Error"
             return False
 
@@ -129,23 +128,29 @@ class CalcView(discord.ui.View):
         self.maybe_update_output()
         self.new_edits_avaible.set()
 
-    @discord.ui.button(label="Clear", style=discord.ButtonStyle.danger, row=0)
-    async def clear(self, item: discord.ui.Item, interaction: discord.Interaction):
-        self.input = "..."
-        self.output = "..."
+    @discord.ui.button(label=ZERO_WIDTH, style=discord.ButtonStyle.grey, row=0)
+    async def empty_button(self, item: discord.ui.Item, interaction: discord.Interaction):
+        await interaction.response.send_message("You found the useless button!", ephemeral=True)
+
+    @discord.ui.button(label="÷", style=discord.ButtonStyle.blurple, row=0)
+    async def divide(self, item: discord.ui.Item, interaction: discord.Interaction):
+        if self.input_reset_ready:
+            self.input = ""
+            self.input_reset_ready = False
+        self.input += "/"
+        self.maybe_update_output()
         self.new_edits_avaible.set()
-        self.input_reset_ready = True
 
-    @discord.ui.button(label="Exit", style=discord.ButtonStyle.danger, row=0)
-    async def exit(self, item: discord.ui.Item, interaction: discord.Interaction):
-        await self.ready.wait()
-        assert self.message is not None
-
-        self.lazy_edit_task.cancel()
-
-        self.stop()
-
-        await self.message.edit(view=ClosedView())
+    @discord.ui.button(label="⌫", style=discord.ButtonStyle.red, row=0)
+    async def backspace(self, item: discord.ui.Item, interaction: discord.Interaction):
+        if self.input_reset_ready:
+            self.input = ""
+            self.input_reset_ready = False
+        self.input = self.input[:-1]
+        if len(self.input) == 0:
+            self.input = "..."
+        self.maybe_update_output()
+        self.new_edits_avaible.set()
 
     @discord.ui.button(label="7", style=discord.ButtonStyle.grey, row=1)
     async def seven(self, item: discord.ui.Item, interaction: discord.Interaction):
@@ -174,14 +179,21 @@ class CalcView(discord.ui.View):
         self.maybe_update_output()
         self.new_edits_avaible.set()
 
-    @discord.ui.button(label="÷", style=discord.ButtonStyle.blurple, row=1)
-    async def divide(self, item: discord.ui.Item, interaction: discord.Interaction):
+    @discord.ui.button(label="×", style=discord.ButtonStyle.blurple, row=1)
+    async def multiply(self, item: discord.ui.Item, interaction: discord.Interaction):
         if self.input_reset_ready:
             self.input = ""
             self.input_reset_ready = False
-        self.input += "/"
+        self.input += "*"
         self.maybe_update_output()
         self.new_edits_avaible.set()
+
+    @discord.ui.button(label="Clear", style=discord.ButtonStyle.danger, row=1)
+    async def clear(self, item: discord.ui.Item, interaction: discord.Interaction):
+        self.input = "..."
+        self.output = "..."
+        self.new_edits_avaible.set()
+        self.input_reset_ready = True
 
     @discord.ui.button(label="4", style=discord.ButtonStyle.grey, row=2)
     async def four(self, item: discord.ui.Item, interaction: discord.Interaction):
@@ -210,14 +222,25 @@ class CalcView(discord.ui.View):
         self.maybe_update_output()
         self.new_edits_avaible.set()
 
-    @discord.ui.button(label="×", style=discord.ButtonStyle.blurple, row=2)
-    async def multiply(self, item: discord.ui.Item, interaction: discord.Interaction):
+    @discord.ui.button(label="-", style=discord.ButtonStyle.blurple, row=2)
+    async def minus(self, item: discord.ui.Item, interaction: discord.Interaction):
         if self.input_reset_ready:
             self.input = ""
             self.input_reset_ready = False
-        self.input += "*"
+        self.input += "-"
         self.maybe_update_output()
         self.new_edits_avaible.set()
+
+    @discord.ui.button(label="Exit", style=discord.ButtonStyle.danger, row=2)
+    async def exit(self, item: discord.ui.Item, interaction: discord.Interaction):
+        await self.ready.wait()
+        assert self.message is not None
+
+        self.lazy_edit_task.cancel()
+
+        self.stop()
+
+        await self.message.edit(view=ClosedView())
 
     @discord.ui.button(label="1", style=discord.ButtonStyle.grey, row=3)
     async def one(self, item: discord.ui.Item, interaction: discord.Interaction):
@@ -246,12 +269,12 @@ class CalcView(discord.ui.View):
         self.maybe_update_output()
         self.new_edits_avaible.set()
 
-    @discord.ui.button(label="-", style=discord.ButtonStyle.blurple, row=3)
-    async def minus(self, item: discord.ui.Item, interaction: discord.Interaction):
+    @discord.ui.button(label="+", style=discord.ButtonStyle.blurple, row=3)
+    async def plus(self, item: discord.ui.Item, interaction: discord.Interaction):
         if self.input_reset_ready:
             self.input = ""
             self.input_reset_ready = False
-        self.input += "-"
+        self.input += "+"
         self.maybe_update_output()
         self.new_edits_avaible.set()
 
@@ -273,19 +296,10 @@ class CalcView(discord.ui.View):
         self.maybe_update_output()
         self.new_edits_avaible.set()
 
-    @discord.ui.button(label="=", style=discord.ButtonStyle.green, row=4)
+    @discord.ui.button(label=EQUALS_LABEL, style=discord.ButtonStyle.green, row=4)
     async def equals(self, item: discord.ui.Item, interaction: discord.Interaction):
         if not self.maybe_update_output():
             self.output = "Math Error"
         self.input_reset_ready = True
 
-        self.new_edits_avaible.set()
-
-    @discord.ui.button(label="+", style=discord.ButtonStyle.blurple, row=4)
-    async def plus(self, item: discord.ui.Item, interaction: discord.Interaction):
-        if self.input_reset_ready:
-            self.input = ""
-            self.input_reset_ready = False
-        self.input += "+"
-        self.maybe_update_output()
         self.new_edits_avaible.set()
