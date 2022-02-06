8bb3dea8bdd23b2a4593c4318316f1219cff5e51
Vexed01 <vex@vexcodes.com>
2022-02-06 11:21:59 +0000

    [Birthday 1.0.3] Catch OverflowError in bdset zemigrate

--- birthday/commands.py
+++ birthday/commands.py
@@ -635,7 +635,10 @@ class BirthdayAdminCommands(MixinMeta):
             for day, users in guild_data.items():
                 for user_id, year in users.items():
                     dt = datetime.datetime.fromordinal(int(day))
-                    dt = dt.replace(year=year or 1)
+                    try:
+                        dt = dt.replace(year=year or 1)
+                    except OverflowError:  # flare's users are crazy
+                        dt = dt.replace(year=1)
 
                     new_data = {
                         "year": dt.year if dt.year != 1 else None,
