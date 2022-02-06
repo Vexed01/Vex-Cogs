b8232640726f226293fc4a29987336e82f32290e
Vexed01 <vex@vexcodes.com>
2022-02-06 11:41:01 +0000

    [Birthday] I can read

--- birthday/commands.py
+++ birthday/commands.py
@@ -385,11 +385,11 @@ class BirthdayAdminCommands(MixinMeta):
         table = Table("Name", "Value", title="Settings for this server")
 
         async with self.config.guild(ctx.guild).all() as conf:
-            channel = ctx.guild.get_channel(conf["channel_id"], "Channel deleted")
-            table.add_row("Channel", channel)
+            channel = ctx.guild.get_channel(conf["channel_id"])
+            table.add_row("Channel", channel.name if channel else "Channel deleted")
 
-            role = ctx.guild.get_role(conf["role_id"], "Role deleted")
-            table.add_row("Role", role)
+            role = ctx.guild.get_role(conf["role_id"])
+            table.add_row("Role", role.name if role else "Role deleted")
 
             time = datetime.datetime.utcfromtimestamp(conf["time_utc_s"]).strftime("%H:%M UTC")
             table.add_row("Time", time)
