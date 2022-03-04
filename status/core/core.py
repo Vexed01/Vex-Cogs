Vexed01/Vex-Cogs

commit bd848529ff0a119b890a3217d4bf4c5fa457209e

[Status 2.5.1] Fix Red 3.5.0 compatibility Now compatible with Red PR #5576

status/core/core.py
@@ -42,7 +42,7 @@ class Status(
     make an issue on the GitHub repo (or even better a PR!).
     """
 
-    __version__ = "2.5.0"
+    __version__ = "2.5.1"
     __author__ = "Vexed#9000"
 
     def __init__(self, bot: Red) -> None:

status/updateloop/utils.py
@@ -87,7 +87,7 @@ async def get_channel_data(bot: Red, c_id: int, settings: ConfChannelSettings) -
         raise NoPermission
 
     if not settings["webhook"]:
-        use_embed = await bot.embed_requested(channel, channel.guild.me)  # type:ignore
+        use_embed = await bot.embed_requested(channel)  # type:ignore
     else:
         use_embed = True
 

commit c475ff49f985064215129644684bb4104727ff69

[Status 2.5.2] Fix the cog
Someone broke it in bd84852, smh

status/core/core.py
@@ -42,7 +42,7 @@ class Status(
     make an issue on the GitHub repo (or even better a PR!).
     """
 
-    __version__ = "2.5.1"
+    __version__ = "2.5.2"
     __author__ = "Vexed#9000"
 
     def __init__(self, bot: Red) -> None:

status/updateloop/utils.py
@@ -87,7 +87,15 @@ async def get_channel_data(bot: Red, c_id: int, settings: ConfChannelSettings) -
         raise NoPermission
 
     if not settings["webhook"]:
-        use_embed = await bot.embed_requested(channel)  # type:ignore
+        try:
+            use_embed = await bot.embed_requested(channel)  # type:ignore
+        except TypeError:  # as of time of writing no way to distinguish between red vers with and
+            # without new code - as for some reason i added this while change in PR form :kappa:
+            use_embed = await bot.embed_requested(channel, channel.guild.me)  # type:ignore
+
+        # TODO: clean out at some point:tm:
+        # thing for when i search for dpy2 stuff:
+        # discord.__version__.startswith("1.")
     else:
         use_embed = True
 