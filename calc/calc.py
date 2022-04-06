d657d81e018ea03ce548b41a6e5fec491c394278
Vexed01 <vex@vexcodes.com>
2022-04-06 15:02:42 +0100

    [Calc 0.0.2] Improve compatibility with dpy

--- calc/calc.py
+++ calc/calc.py
@@ -8,7 +8,7 @@ from .view import CalcView
 class Calc(commands.Cog):
     """Calculate simple mathematical expressions."""
 
-    __version__ = "0.0.1"
+    __version__ = "0.0.2"
     __author__ = "Vexed#9000"
 
     def __init__(self, bot: Red) -> None:
