fc066b524b1bd2d4af2c7a4071b74968852324fe
Vexed <gh.vexed@gmail.com>
2023-04-28 16:26:44 +0100

    [GitHub] Remove cog, in favour of ghissues

--- github/__init__.py
+++ github/__init__.py
@@ -1,20 +1,9 @@
-import contextlib
-import importlib
-import json
-from pathlib import Path
-
-from redbot.core import VersionInfo
 from redbot.core.bot import Red
-
-from . import vexutils
-from .github import GitHub
-from .vexutils.meta import out_of_date_check
-
-with open(Path(__file__).parent / "info.json") as fp:
-    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
+from redbot.core.errors import CogLoadError
 
 
 async def setup(bot: Red) -> None:
-    cog = GitHub(bot)
-    await out_of_date_check("github", cog.__version__)
-    await bot.add_cog(cog)
+    raise CogLoadError(
+        "This cog has been replaced by my `ghissues` cog. Please uninstall this cog and install "
+        "that instead."
+    )
