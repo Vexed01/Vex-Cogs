11069c77ba502082cdbb75c25bc77846ec3426ec
Vexed <gh.vexed@gmail.com>
2023-05-01 19:40:28 +0100

    Remove CovidGraph cog
    * remove vexutils from github
    * and remove cogs from other spots

--- covidgraph/__init__.py
+++ covidgraph/__init__.py
@@ -1,20 +1,8 @@
-import contextlib
-import importlib
-import json
-from pathlib import Path
-
-from redbot.core import VersionInfo
-from redbot.core.bot import Red
-
-from . import vexutils
-from .covidgraph import CovidGraph
-from .vexutils.meta import out_of_date_check
-
-with open(Path(__file__).parent / "info.json") as fp:
-    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
+from redbot.core.errors import CogLoadError
 
 
-async def setup(bot: Red) -> None:
-    cog = CovidGraph(bot)
-    await out_of_date_check("covidgraph", cog.__version__)
-    await bot.add_cog(cog)
+async def setup(_):
+    raise CogLoadError(
+        "This cog will not be updated to be compatible with Red 3.5 due to irrelevance and data "
+        "issues, and has therefore been removed."
+    )
