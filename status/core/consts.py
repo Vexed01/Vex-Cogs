e1776b55f12d976db567c64b0d11d15cc563616a
Vexed <gh.vexed@gmail.com>
2023-07-13 19:46:14 +0100

    [Status] Update DigitalOcean's ID closes #118

--- status/core/consts.py
+++ status/core/consts.py
@@ -41,91 +41,76 @@ ICON_BASE = "https://static.vexcodes.com/v1/status_icons/{}.png"
 FEEDS = {
     "discord": {
         "url": "https://discordstatus.com/",
-        # c-spell: disable-next-line
         "id": "srhpyqt94yxb",
         "friendly": "Discord",
     },
     "github": {
         "url": "https://www.githubstatus.com/",
-        # c-spell: disable-next-line
         "id": "kctbh9vrtdwd",
         "friendly": "GitHub",
     },
     "cloudflare": {
         "url": "https://www.cloudflarestatus.com/",
-        # c-spell: disable-next-line
         "id": "yh6f0r4529hb",
         "friendly": "Cloudflare",
     },
     "python": {
         "url": "https://status.python.org/",
-        # c-spell: disable-next-line
         "id": "2p66nmmycsj3",
         "friendly": "Python",
     },
     "twitter_api": {
         "url": "https://api.twitterstat.us/",
-        # c-spell: disable-next-line
         "id": "zjttvm6ql9lp",
         "friendly": "Twitter API",
     },
     "statuspage": {
         "url": "https://metastatuspage.com/",
-        # c-spell: disable-next-line
         "id": "y2j98763l56x",
         "friendly": "Statuspage",
     },
     "zoom": {
         "url": "https://status.zoom.us/",
-        # c-spell: disable-next-line
         "id": "14qjgk812kgk",
         "friendly": "Zoom",
     },
     "oracle_cloud": {
         "url": "https://ocistatus.oraclecloud.com/",
-        # c-spell: disable-next-line
         "id": "2bsbdh54lcgq",
         "friendly": "Oracle Cloud",
     },
     "epic_games": {
         "url": "https://status.epicgames.com/",
-        # c-spell: disable-next-line
         "id": "ft308v428dv3",
         "friendly": "Epic Games",
     },
     "digitalocean": {
         "url": "https://status.digitalocean.com/",
-        # c-spell: disable-next-line
-        "id": "s2k7tnzlhrpw",
+        "id": "w4cz49tckxhp",
         "friendly": "DigitalOcean",
     },
     "reddit": {
         "url": "https://www.redditstatus.com/",
-        # c-spell: disable-next-line
         "id": "2kbc0d48tv3j",
         "friendly": "Reddit",
     },
     "sentry": {
         "url": "https://status.sentry.io/",
-        # c-spell: disable-next-line
         "id": "t687h3m0nh65",
         "friendly": "Sentry",
     },
     "geforcenow": {
         "url": "https://status.geforcenow.com/",
-        # c-spell: disable-next-line
         "id": "2bdwmtrb0hg9",
         "friendly": "GeForce NOW",
     },
     "wikimedia": {
         "url": "https://www.wikimediastatus.net/",
-        # c-spell: disable-next-line
         "id": "nnqjzz7cd4tj",
         "friendly": "Wikimedia",
     },
     "twitch": {
         "url": "https://status.twitch.tv/",
-        # c-spell: disable-next-line
         "id": "yfj40zdsk34s",
         "friendly": "Twitch",
     },
